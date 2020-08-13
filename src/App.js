44f8e195feb27655a8f4e4aad49320cc2b680341
dawum nam <dawumnam@gmail.com>
2020-08-12 19:39:07 -0800

    mo

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ import { fetchData } from './api';
 
 class App extends React.Component {
     state = {
-        data: {}
+        data: {},
+        country: '',
     }
+
 
     async componentDidMount() {
         const fetchedData = await fetchData();    
         this.setState({data:fetchedData})
     }
 
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);   
+        this.setState({data: fetchedData, country: country})
+    }
+
     render() {
-        const { data } = this.state
+        const { data, country } = this.state
         return (
          <div className={styles.container}>
              <Cards data={data}/>
-             <Chart />
-             <CountryPicker />
-             
+             <Chart data={data,country}/>
+             <CountryPicker handleCountryChange={this.handleCountryChange}/>
          </div>
         )
     }
